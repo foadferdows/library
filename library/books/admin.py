ef4400c5be0bddee7ea2e6a294868e42630ae02c
Foad Ferdows <foadferdows@Foads-MacBook-Air.local>
2025-09-08 17:22:57 +0330

    feat(admin): register models with search and filters

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Author, Book, Favorite, UserExtra
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "country", "sex", "birth_date")
+    search_fields = ("name", "bio")
+    list_filter = ("country", "sex")
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "category", "price", "published_date")
+    search_fields = ("title", "description", "author__name")
+    list_filter = ("category", "published_date")
+    
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ("user", "book", "date")
+    search_fields = ("user__username", "book__title")
+
+@admin.register(UserExtra)
+class UserExtraAdmin(admin.ModelAdmin):
+    list_display = ("user", "sex", "nationalcode")
+    search_fields = ("user__username", "nationalcode")
